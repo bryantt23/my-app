37bde8464b25bf67ef24e42bb6d469df51007673
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-20 22:43:31 -0800

    Use map

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,15 +6,9 @@ interface ContentProps {
 const Content: React.FC<ContentProps> = ({ courseParts }) => {
     return (
         <div>
-            <p>
-                {courseParts[0].name} {courseParts[0].exerciseCount}
-            </p>
-            <p>
-                {courseParts[1].name} {courseParts[1].exerciseCount}
-            </p>
-            <p>
-                {courseParts[2].name} {courseParts[2].exerciseCount}
-            </p>
+            {courseParts.map((coursePart, i) => <p key={i}>
+                {coursePart.name} {coursePart.exerciseCount}
+            </p>)}
         </div>
     )
 }
